55cec578faf08a2a75abfaadea43b0c4fa332f31
Alexander Draginkii <morq000@gmail.com>
2021-11-09 23:12:56 +0300

    moved starter tasks

---
+++ starter_kit/3.1 shop_queue.sol
@@ -0,0 +1,50 @@
+pragma ton-solidity >= 0.50;
+pragma AbiHeader expire;
+
+contract  ShopQueue {
+
+	string[] public queue;
+
+	constructor() public {
+		queue = ["Curt", "Janet", "Sid"];
+		tvm.accept();
+	}
+
+	modifier checkOwnerAndAccept() {
+		require(tvm.pubkey() == msg.pubkey(), 105, "Only owner can execute");
+		tvm.accept();
+		_;
+	}
+
+	// Function to output current queue qhile running on a test net
+	function outputQueue() public view checkOwnerAndAccept returns (string[]) {
+		return queue;
+	}
+
+	// Function to add new member to the end of queue
+	function join(string new_name) public checkOwnerAndAccept returns (string[]) {
+		queue.push(new_name);
+
+		return queue;
+	}
+
+	// Function to pop out the first member of the queue and move other values
+	function callNext() public checkOwnerAndAccept returns (string[]) {
+		// Check if array is already empty
+		require(queue.length > 0, 107, "The queue is already empty");
+
+		// Delete element at index 0
+		delete queue[0];
+
+		// Move all elements one step left
+		for (uint256 index = 0; index < queue.length - 1; index++) {
+			queue[index] = queue[index+1];
+		}
+		
+		// Pop out the last element as it is already moved
+		queue.pop();
+
+		return queue;
+	}
+	
+}
